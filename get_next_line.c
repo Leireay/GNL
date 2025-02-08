8dc19f54b9d907c8451e48d4e611ccafcecc3dfe
Leireay <163833943+Leireay@users.noreply.github.com>
2025-02-08 08:43:12 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,112 +1,42 @@
+#include <limits.h>
 #include "get_next_line.h"
 
-char	*ft_get_line(char *buffer)
+static void	clear_buffer(char buffer[BUFFER_SIZE + 1])
 {
-	int		i;
-	char	*str;
+	size_t	i;
 
 	i = 0;
-	if (!buffer[i])
-		return (NULL);
-	while (buffer[i] && buffer[i] != '\n')
+	while (i < BUFFER_SIZE + 1)
+	{
+		buffer[i] = '\0';
 		i++;
-	str = ft_substr(buffer, 0, i + ft_endl(buffer));
-	if (!str)
-	{
-		free(str);
-		return (NULL);
 	}
-	return (str);
-}
-
-char	*ft_new_str(char *buffer)
-{
-	int		i;
-	int		j;
-	char	*str;
-
-	i = 0;
-	while (buffer[i] && buffer[i] != '\n')
-		i++;
-	if (!buffer[i])
-	{
-		free(buffer);
-		return (NULL);
-	}
-	str = malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
-	if (!str)
-	{
-		free(str);
-		return (NULL);
-	}
-	i++;
-	j = 0;
-	while (buffer[i])
-		str[j++] = buffer[i++];
-	str[j] = '\0';
-	free(buffer);
-	return (str);
-}
-
-char	*ft_read_str(int fd, char *buffer)
-{
-	char	*s;
-	int		bytes;
-
-	s = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!s)
-		return (NULL);
-	bytes = 1;
-	while (!ft_strchr(buffer, '\n') && bytes != 0)
-	{
-		bytes = read(fd, s, BUFFER_SIZE);
-		if (bytes < 0)
-		{
-			free(s);
-			return (NULL);
-		}
-		s[bytes] = '\0';
-		buffer = ft_strjoin(buffer, s);
-	}
-	free(s);
-	return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*line;
-	static char	*buffer;
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*output;
+	size_t		i;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	buffer = ft_read_str(fd, buffer);
-	if (!buffer)
+	if (fd < 0 || fd > FOPEN_MAX || BUFFER_SIZE < 0)
 		return (NULL);
-	line = ft_get_line(buffer);
-	buffer = ft_new_str(buffer);
-	return (line);
-}
-
-/*int    main(void)
-{
-    char    *line;
-    int        i;
-    int        fd;
-    fd = open("file1.txt", O_RDONLY);
-    i = 1;
-    while (i < 7)
-    {
-        line = get_next_line(fd);
-        printf("line [%02d]: %s", i, line);
-        free(line);
-        i++;
-    }
-	if (i == -1)
+	output = NULL;
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n' && buffer[i + 1])
+		strreplace(&output, strnjoin(output, buffer + i + 1, BUFFER_SIZE));
+	clear_buffer(buffer);
+	while (read(fd, buffer, BUFFER_SIZE) > 0)
 	{
-		printf("<ERROR>\n");
-		close(fd);
-		return (-1);
+		i = 0;
+		while (buffer[i] && buffer[i] != '\n')
+			i++;
+		strreplace(&output, strnjoin(output, buffer, i + 1));
+		if (buffer[i] == '\n')
+			break ;
+		clear_buffer(buffer);
 	}
-    close(fd);
-    return (0);
-}*/
+	return (output);
+}
