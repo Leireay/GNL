b1aee5936fd50d3d94269682ef9dcac2c714f164
Leireay <163833943+Leireay@users.noreply.github.com>
2025-02-08 08:01:31 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -10,7 +10,7 @@ size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strjoin(char *buffer, char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-int		ft_endl(char *str);
+int    ft_endl(char *str);
 char	*ft_get_line(char *buffer);
 char	*ft_new_str(char *buffer);
 char	*ft_read_str(int fd, char *buffer);
